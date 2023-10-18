3b02e4b44eb3417af690ef67941cbb2a9292d68e
Huclark <vhuclark@gmail.com>
2023-10-18 16:09:06 +0000

    Betty errors fixed

--- my_shell.c
+++ my_shell.c
@@ -1,16 +1,16 @@
 #include "shell.h"
 
 /**
-* main - main function for shell program
-* @argc: Argument count
-* @argv: Argument vector
-* Return: The exit code (loop_status)
-*/
+ * main - main function for shell program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: The exit code (loop_status)
+ */
 
 int main(int argc, char **argv)
 {
-        (void)argc;
+	(void)argc;
 
-        return (shell_loop(argv));
+	return (shell_loop(argv));
 }
 
--- print_functions_1.c
+++ print_functions_1.c
@@ -1,28 +1,28 @@
 #include "shell.h"
 
 /**
-* put_char - Prints a char character to standard output
-* @ch: The character to print
-* Return: 1 on success and -1 on failure
-*/
+ * put_char - Prints a char character to standard output
+ * @ch: The character to print
+ * Return: 1 on success and -1 on failure
+ */
 int put_char(char ch)
 {
-        int ret_value = write(1, &ch, 1);
+	int ret_value = write(1, &ch, 1);
 
-        return (ret_value);
+	return (ret_value);
 }
 
 
 /**
-* e_put_char - Prints a char character to standard error
-* @ch: The character to print
-* Return: 1 on success and -1 on failure
-*/
+ * e_put_char - Prints a char character to standard error
+ * @ch: The character to print
+ * Return: 1 on success and -1 on failure
+ */
 int e_put_char(char ch)
 {
-        int ret_value = write(2, &ch, 1);
+	int ret_value = write(2, &ch, 1);
 
-        return (ret_value);
+	return (ret_value);
 }
 
 
@@ -30,37 +30,37 @@ int e_put_char(char ch)
 /**
  * cust_puts - replicates the puts function
  * @string: string input to print
-*/
+ */
 void cust_puts(char *string)
 {
-        int idx = 0;
+	int idx = 0;
 
-        if (string == NULL)
-                return;
-        while (string[idx] != '\0')
-        {
-                put_char(string[idx]);
-                idx += 1;
-        }
+	if (string == NULL)
+		return;
+	while (string[idx] != '\0')
+	{
+		put_char(string[idx]);
+		idx += 1;
+	}
 }
 
 
 /**
  * e_cust_puts - replicates the puts function
  * @string: string input to print
-*/
+ */
 
 void e_cust_puts(char *string)
 {
-        int idx = 0;
+	int idx = 0;
 
-        if (string == NULL)
-                return;
-        while (string[idx] != '\0')
-        {
-                e_put_char(string[idx]);
-                idx += 1;
-        }
+	if (string == NULL)
+		return;
+	while (string[idx] != '\0')
+	{
+		e_put_char(string[idx]);
+		idx += 1;
+	}
 }
 
 
@@ -72,35 +72,37 @@ void e_cust_puts(char *string)
  */
 int print_numbers(int num_to_print, int file_desc)
 {
-        int idx, __counter = 0;
-        unsigned int absolute, curr_num;
-        int (*putchar_ptr)(char) = put_char;
+	int idx, __counter = 0;
 
-        if (file_desc == STDERR_FILENO)
-                putchar_ptr = e_put_char;
+	unsigned int absolute, curr_num;
 
-        if (num_to_print < 0)
-        {
-                absolute = -num_to_print;
-                putchar_ptr('-');
-                __counter++;
-        }
-        else
-                absolute = num_to_print;
-        curr_num = absolute;
-        for (idx = 1000000000; idx > 1; idx /= 10)
-        {
-                if (absolute / idx)
-                {
-                        putchar_ptr('0' + curr_num / idx);
-                        __counter++;
-                }
-                curr_num %= idx;
-        }
-        putchar_ptr('0' + curr_num);
-        __counter++;
+	int (*putchar_ptr)(char) = put_char;
 
-        return (__counter);
+	if (file_desc == STDERR_FILENO)
+		putchar_ptr = e_put_char;
+
+	if (num_to_print < 0)
+	{
+		absolute = -num_to_print;
+		putchar_ptr('-');
+		__counter++;
+	}
+	else
+		absolute = num_to_print;
+	curr_num = absolute;
+	for (idx = 1000000000; idx > 1; idx /= 10)
+	{
+		if (absolute / idx)
+		{
+			putchar_ptr('0' + curr_num / idx);
+			__counter++;
+		}
+		curr_num %= idx;
+	}
+	putchar_ptr('0' + curr_num);
+	__counter++;
+
+	return (__counter);
 }
 
 
